779e3ec99c242afcde34607f9dc3103ffc316aa8
rajkumar-012 <iknrkumar012@gmail.com>
2025-11-11 16:02:46 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import 'expo-dev-client';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import { HistoryScreen } from './src/screens/HistoryScreen';
+import { HomeScreen } from './src/screens/HomeScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+  History: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="History"
+            component={HistoryScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  );
+}
+
